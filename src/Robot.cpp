324d9edd318af16cbe97671dd5268118bd403c74
vanluwin <richardsonsantiago@ufrn.edu.br>
2019-08-24 14:38:20 -0300

    Classe do robo com os movimentos básicos

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,148 +1,93 @@
 #include "Robot.hpp"
 
-Motor::Motor() {}
-
-Motor::~Motor() {}
-
-void Motor::setupInterruptHandler(void (*ISR)(void), int value) {
-    attachInterrupt(
-        digitalPinToInterrupt(this->encoderPin),
-        ISR,
-        value
-    );
-}
-
-void Motor::handleInterrupt(void) {
-    this->cont += this->side;
-
-    if (this->cont == this->goal) {
-        this->cont = 0;
-        this->stop();
-    }
-}
-
-void Motor::setPins(int _encoderPin, int _pin1, int _pin2) {
-    this->encoderPin = _encoderPin;
-    this->pin1 = _pin1;
-    this->pin2 = _pin2;
-
-    pinMode(this->encoderPin, INPUT_PULLUP);
-    pinMode(this->pin1, OUTPUT);
-    pinMode(this->pin2, OUTPUT);
-}
-
-void Motor::forward(long int _goal) {
-    this->side = 1;
-    this->cont = 0;
-    this->goal = _goal;
-
-    digitalWrite(this->pin1, HIGH);
-    digitalWrite(this->pin2, LOW);
-
-    //Serial.println("Starting forward");
-}
-
-void Motor::backward(long int _goal) {
-    this->side = 1;
-    this->cont = 0;
-    this->goal = _goal;
-
-    digitalWrite(this->pin1, LOW);
-    digitalWrite(this->pin2, HIGH);
-
-    //Serial.println("Starting backward");
-}
-
-void Motor::stop() {
-    digitalWrite(this->pin1, LOW);
-    digitalWrite(this->pin2, LOW);
-
-    //Serial.println("Stopping");
-}
-
 Robot::Robot() {
     // Inicia os pinos dos motores
-    this->frontLeft.setPins(2, 5, 4);
-    this->frontRight.setPins(3, 6, 7);
-    this->backLeft.setPins(18, 8, 9);
-    this->backRight.setPins(19, 10, 11);
+    frontLeft.setPins(2, 5, 4);
+    frontRight.setPins(3, 6, 7);
+    backLeft.setPins(18, 8, 9);
+    backRight.setPins(19, 10, 11);
+
+    frontLeft.begin();
+    frontRight.begin();
+    backLeft.begin();
+    backRight.begin();
 }
 
-void Robot::forward(long int goal) {
-    this->frontLeft.forward(goal);
-    this->frontRight.forward(goal);
-    this->backLeft.forward(goal);
-    this->backRight.forward(goal);
+void Robot::forward(unsigned long int goal) {
+    frontLeft.forward(goal);
+    frontRight.forward(goal);
+    backLeft.forward(goal);
+    backRight.forward(goal);
 }
 
-void Robot::backward(long int goal) {
-    this->frontLeft.backward(goal);
-    this->frontRight.backward(goal);
-    this->backLeft.backward(goal);
-    this->backRight.backward(goal);
+void Robot::backward(unsigned long int goal) {
+    frontLeft.backward(goal);
+    frontRight.backward(goal);
+    backLeft.backward(goal);
+    backRight.backward(goal);
 }
 
-void Robot::sidewaysRight(long int goal) {
-    this->frontLeft.forward(goal);
-    this->frontRight.backward(goal);
-    this->backLeft.backward(goal);
-    this->backRight.forward(goal);
+void Robot::sidewaysRight(unsigned long int goal) {
+    frontLeft.forward(goal);
+    frontRight.backward(goal);
+    backLeft.backward(goal);
+    backRight.forward(goal);
 }
 
-void Robot::sidewaysLeft(long int goal) {
-    this->frontLeft.backward(goal);
-    this->frontRight.forward(goal);
-    this->backLeft.forward(goal);
-    this->backRight.backward(goal);
+void Robot::sidewaysLeft(unsigned long int goal) {
+    frontLeft.backward(goal);
+    frontRight.forward(goal);
+    backLeft.forward(goal);
+    backRight.backward(goal);
 }
 
-void Robot::rotateLeft(long int goal) {
-    this->frontLeft.backward(goal);
-    this->frontRight.forward(goal);
-    this->backLeft.backward(goal);
-    this->backRight.forward(goal);  
+void Robot::rotateLeft(unsigned long int goal) {
+    frontLeft.backward(goal);
+    frontRight.forward(goal);
+    backLeft.backward(goal);
+    backRight.forward(goal);
 }
 
-void Robot::rotateRight(long int goal) {
-    this->frontLeft.forward(goal);
-    this->frontRight.backward(goal);
-    this->backLeft.forward(goal);
-    this->backRight.backward(goal);
+void Robot::rotateRight(unsigned long int goal) {
+    frontLeft.forward(goal);
+    frontRight.backward(goal);
+    backLeft.forward(goal);
+    backRight.backward(goal);
 }
 
-void Robot::moveRightForward(long int goal) {
-    this->frontLeft.forward(goal);
-    this->frontRight.stop();
-    this->backLeft.stop();
-    this->backRight.forward(goal);
+void Robot::moveRightForward(unsigned long int goal) {
+    frontLeft.forward(goal);
+    frontRight.stop();
+    backLeft.stop();
+    backRight.forward(goal);
 }
 
-void Robot::moveRightBackward(long int goal) {
-    this->frontLeft.stop();
-    this->frontRight.backward(goal);
-    this->backLeft.backward(goal);
-    this->backRight.stop();
+void Robot::moveRightBackward(unsigned long int goal) {
+    frontLeft.stop();
+    frontRight.backward(goal);
+    backLeft.backward(goal);
+    backRight.stop();
 }
 
-void Robot::moveLeftForward(long int goal) {
-    this->frontLeft.stop();
-    this->frontRight.forward(goal);
-    this->backLeft.forward(goal);
-    this->backRight.stop();
+void Robot::moveLeftForward(unsigned long int goal) {
+    frontLeft.stop();
+    frontRight.forward(goal);
+    backLeft.forward(goal);
+    backRight.stop();
 }
 
-void Robot::moveLeftBackward(long int goal) {
-    this->frontLeft.backward(goal);
-    this->frontRight.stop();
-    this->backLeft.stop();
-    this->backRight.backward(goal);
+void Robot::moveLeftBackward(unsigned long int goal) {
+    frontLeft.backward(goal);
+    frontRight.stop();
+    backLeft.stop();
+    backRight.backward(goal);
 }
 
 void Robot::stop() {
-    this->frontLeft.stop();
-    this->frontRight.stop();
-    this->backLeft.stop();
-    this->backRight.stop();
+    frontLeft.stop();
+    frontRight.stop();
+    backLeft.stop();
+    backRight.stop();
 }
 
 Robot::~Robot() {}
--- src/Robot.hpp
+++ src/Robot.hpp
@@ -1,51 +1,34 @@
 #ifndef ROBOT_HPP
 #define ROBOT_HPP
 
-#include "Arduino.h"
-
-class Motor {
-    public: 
-        int encoderPin;
-        int pin1, pin2;
-
-        volatile long unsigned int cont;
-        int goal;
-
-        int side;
-    
-    
-        Motor();
-        ~Motor();
-
-        void setupInterruptHandler(void (*ISR)(void), int value);
-        void handleInterrupt(void);
-
-        void setPins(int _encoderPin, int _pin1, int _pin2);
-       
-        void forward(long int _goal);
-        void backward(long int _goal);
-        void stop();
-};
+#include "Motor.hpp"
 
 class Robot {
-    public:
-        Motor frontLeft, frontRight, backLeft, backRight;
-        
+    private:
+        Motor frontLeft{0}, frontRight{1}, backLeft{2}, backRight{3};
+    
+    public:    
         Robot();
+        ~Robot();
 
         void stop();
-        void forward(long int goal); 
-        void backward(long int goal);
-        void sidewaysRight(long int goal);
-        void sidewaysLeft(long int goal);
-        void rotateLeft(long int goal);
-        void rotateRight(long int goal);
-        void moveRightForward(long int goal);
-        void moveRightBackward(long int goal);
-        void moveLeftForward(long int goal);
-        void moveLeftBackward(long int goal);
+        void forward(unsigned long int goal); 
+        void backward(unsigned long int goal);
+        
+        // Movimento lateral
+        void sidewaysRight(unsigned long int goal);
+        void sidewaysLeft(unsigned long int goal);
+        
+        // Rotações
+        void rotateLeft(unsigned long int goal);
+        void rotateRight(unsigned long int goal);
 
-        ~Robot();
+        // Diagonais
+        void moveRightForward(unsigned long int goal);
+        void moveRightBackward(unsigned long int goal);
+        void moveLeftForward(unsigned long int goal);
+        void moveLeftBackward(unsigned long int goal);
+
 };
 
 #endif // ROBOT_HPP
