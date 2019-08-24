18e9bc80ea0d6d59d8aa1539d6188346b2af0747
vanluwin <richardsonsantiago@ufrn.edu.br>
2019-08-24 14:37:56 -0300

    Classe do motor com interrupt como método

---
+++ src/Motor.hpp
@@ -0,0 +1,40 @@
+#ifndef MOTOR_HPP
+#define MOTOR_HPP
+
+#include <Arduino.h>
+
+class Motor {
+    private: 
+        uint8_t encoderPin;
+        uint8_t pin1, pin2;
+
+        unsigned long int goal;
+        volatile unsigned long int cont;
+
+        uint8_t whichISR;
+        static Motor* instance0;
+        static Motor* instance1;
+        static Motor* instance2;
+        static Motor* instance3;
+
+        static void isr0();
+        static void isr1();
+        static void isr2();
+        static void isr3();
+
+        void handleInterrupt();
+
+    public:
+        Motor(const byte _whichISR);
+        ~Motor();
+
+        void setPins(uint8_t _encoderPin, uint8_t _pin1, uint8_t _pin2);
+        void begin();
+
+        void forward(unsigned long int _goal);
+        void backward(unsigned long int _goal);
+        void stop();
+
+};
+
+#endif // MOTOR_H
