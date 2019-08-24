1227a3c8398882b9b6b7571ff3e5025daeba1147
vanluwin <richardsonsantiago@ufrn.edu.br>
2019-08-24 14:37:29 -0300

    Forward test

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,20 @@
 #include "Robot.hpp"
-#define CALL_INTERVAL 2000
+#define CALL_INTERVAL 4000
 
 Robot boberto;
 unsigned long int lastCall = 0;
 
 void setup() {
     Serial.begin(9600);
-
-    boberto.frontLeft.setupInterruptHandler(
-        []() { boberto.frontLeft.handleInterrupt(); }, 
-        CHANGE
-    );
-    boberto.frontRight.setupInterruptHandler(
-        []() { boberto.frontRight.handleInterrupt(); }, 
-        CHANGE
-    );
-    boberto.backLeft.setupInterruptHandler(
-        []() { boberto.backLeft.handleInterrupt(); }, 
-        CHANGE
-    );
-    boberto.backRight.setupInterruptHandler(
-        []() { boberto.backRight.handleInterrupt(); }, 
-        CHANGE
-    );
+    
+    Serial.println(":: Ready ::");
 }
 
 void loop() {
     if(millis() - lastCall >= CALL_INTERVAL) {
         
-        boberto.forward(1000);
+        boberto.forward(3000);
     
         lastCall = millis();
-    } 
+    }
 }
