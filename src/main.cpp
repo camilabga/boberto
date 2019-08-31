4d783b5360d8c29fc03dc37f809f4a32db9db1a9
vanluwin <richardsonsantiago@ufrn.edu.br>
2019-08-31 15:44:20 -0300

    Teste do sensor de distancia

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,28 @@
+#include <Wire.h>
 #include "Robot.hpp"
 #include "Claw.hpp"
+#include "DistanceSensor.hpp"
 #define CALL_INTERVAL 4000
 
 Robot boberto;
 Claw claw(26, 27, 28, 29);
+DistanceSensor lidar;
+
 unsigned long int lastCall = 0;
 
 void setup() {
+    Wire.begin();
     Serial.begin(9600);
+    lidar.begin();
 
     Serial.println(":: Ready ::");
 }
 
 void loop() {
-    claw.goUp();
-    delay(1000);
-    claw.goHome();
+    Serial.print("Container gap? ");
+    Serial.print(lidar.getContainerGap());
+    Serial.print("  ");
+    Serial.println(lidar.getDistance());
+
     delay(1000);
 }
