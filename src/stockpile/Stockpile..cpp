20e11f9de9fe1e6862a8add0165b2caf22cf3bbf
alvaropfn <alvaropfn@gmail.com>
2019-09-14 22:12:18 -0300

    finishing stockpile class

--- src/stockpile/Stockpile..cpp
+++ src/stockpile/Stockpile..cpp
@@ -3,5 +3,37 @@
 Stockpile::~Stockpile() {}
 
 Stockpile::Stockpile() {
-  Serial.println("it's a sucess");
+    emptyPiles();
+}
+
+void Stockpile::emptyPiles() {
+    memset(blueHeight,0,sizeof(uint8_t));
+    memset(greenHeight,0,sizeof(uint8_t));
+}
+
+void Stockpile::updateStockpile(SHIP color){
+    if(color == greenShip) {
+        if(greenHeight[greenIndex] >= MAX_H){
+            greenIndex += 1;
+        }
+        greenHeight[greenIndex] = greenHeight[greenIndex] + 1;
+        
+    } else {
+       if(greebluenHeight[blueIndex] >= MAX_H){
+            blueIndex += 1;
+        }
+        blueHeight[blueIndex] = blueHeight[blueIndex] + 1;
+    }
+}
+
+PILE Stockpile::getStockpile(SHIP color) {
+    if(color == greenShip){
+        pile.index = 0;
+        pile.height = 1;
+    } else {
+        pile.index = 1;
+        pile.height = 0;
+}
+
+return pile;
 }
--- src/stockpile/Stockpile.hpp
+++ src/stockpile/Stockpile.hpp
@@ -3,18 +3,43 @@
 
 #include <Arduino.h>
 
+#define MAX_H 5
+
+struct PILE {
+int index;
+uint8_t height;
+};
+
+enum SHIP {
+    blueShip,
+    greenShip
+};
+
 class Stockpile {
-  public:
-    Stockpile();
-    ~Stockpile();
+    private:
+        // representation of the ships
+        int greenIndex = 0;
+        int blueIndex = 0;
+        uint8_t greenHeight[MAX_H];
+        uint8_t blueHeight[MAX_H];
 
-    int blue;
-    int green;
+        
+        // variable representation of te actual state of the ships
+        PILE pile;
 
-    // print(char color) {
-    //   if(color == 'g')
-    //   {}
-    // }
+        // fill the ships with empty values
+        void emptyPiles();
+        
+    public:
+        Stockpile();
+        ~Stockpile();
+
+        // update the index and height of a container pile int the rigth ship
+        void updateStockpile(SHIP color);
+
+        // recover the next position where to put the contaienr in color passed ship
+        PILE getStockpile(SHIP);
+
 };
 
 #endif  // STOCK_HPP
